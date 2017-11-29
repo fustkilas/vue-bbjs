d2c775ca7e5f74b8d5e9326e0e63de99d9efaa0c
Fust Kilas <fustkilas@gmail.com>
2017-11-29 11:28:40 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# vue-bbjs
+
+brunch - babel - jade - stylus with vue
+
+including:
+- vue-router
+- auto-reload-brunch
+- clean-css-brunch
+- uglify-js-brunch
---
+++ app/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        msg: 'vue-bbjs'
+      }
+    }
+  }
+
+</script>
