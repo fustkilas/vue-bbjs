ff2641c269d4134a8c77260049fe0f66096282dd
Fust Kilas <fustkilas@gmail.com>
2017-12-11 15:28:35 +0100

    added tachyons by default

--- brunch-config.js
+++ brunch-config.js
@@ -1,18 +1,12 @@
 module.exports = {
-  files: {
-    javascripts: {
-      joinTo: {
-        'vendor.js': /^(?!app)/,
-        'app.js': /^app/
-      }
+    files: {
+        javascripts: {joinTo: 'app.js'},
+        stylesheets: {joinTo: 'app.css'},
     },
-    templates: {
-      joinTo: 'app.js'
+    plugins: {
+        babel: {
+            presets: ['es2015']
+        }
     }
-  },
-  plugins: {
-    babel: {
-      presets: ['es2015']
-    }
-  }
 }
+
